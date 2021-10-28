62e3dc9568fd5df3a4bd22121f9aa6c7793bba77
Pavel Torbeev <32488372+pavel-trbv@users.noreply.github.com>
2021-10-28 03:21:18 +0300

    Added get message stats and format stats methods

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	counter "github.com/pavel-trbv/vk-message-counter"
+	"log"
+	"os"
+	"strconv"
+)
 
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("error load .env")
+		return
+	}
+
+	client := counter.NewCounter(os.Getenv("VK_TOKEN"))
+
+	chatId, err := strconv.Atoi(os.Getenv("VK_CHAT_ID"))
+	if err != nil {
+		log.Fatalf("error convert chat_id from env to int: %s", err.Error())
+	}
+
+	stats, err := client.GetMessageStats(chatId, true)
+	if err != nil {
+		log.Fatal(err.Error())
+		return
+	}
+
+	prettyStats := stats.Format()
+	fmt.Println(prettyStats)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/pavel-trbv/vk-message-counter
 
 go 1.17
+
+require github.com/joho/godotenv v1.4.0 // indirect
