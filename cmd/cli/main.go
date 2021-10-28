b9a625e124c247eab634bb604e07e3f24eef9e57
Pavel Torbeev <32488372+pavel-trbv@users.noreply.github.com>
2021-10-28 16:59:48 +0300

    Added cli endpoint

---
+++ cmd/cli/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	counter "github.com/pavel-trbv/vk-message-counter"
+	"log"
+	"strconv"
+)
+
+func main() {
+	token := flag.String("token", "", "")
+	chatId := flag.String("chat_id", "", "")
+	flag.Parse()
+
+	if token == nil || chatId == nil {
+		log.Fatal("empty arguments")
+	}
+
+	chatIdInt, err := strconv.Atoi(*chatId)
+	if err != nil {
+		log.Fatalf("error convert chat_id from env to int: %s", err.Error())
+	}
+
+	client := counter.NewCounter(*token)
+	stats, err := client.GetMessageStats(chatIdInt, true)
+	if err != nil {
+		log.Fatalf("error occured while getting message stats: %s", err.Error())
+	}
+
+	prettyStats := stats.Format()
+	fmt.Println(prettyStats)
+}
