3595eb055115c16c2932c6d424a7a765ba08073b
Pavel Torbeev <32488372+pavel-trbv@users.noreply.github.com>
2021-11-16 01:27:11 +0300

    Fixed 'about' block header

--- resources/components/About.tsx
+++ resources/components/About.tsx
@@ -4,7 +4,7 @@ import {Heading, Link, Text} from '@chakra-ui/react';
 const About: FC = () => {
     return (
         <>
-            <Heading mb="4">What?</Heading>
+            <Heading mb="4">About</Heading>
             <Text lineHeight="6" mb="4">
                 This is a simple application that allows you to get statistics of chat messages on the&nbsp;
                 <Link href="https://vk.com" color="blue.500">vk.com</Link> social network.
